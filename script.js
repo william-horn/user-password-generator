da5c3c3af9922c9ccf41af709357e6b8e932eaab
William Horn <williamjosephhorn@gmail.com>
2022-03-01 14:39:20 -0500

    fixed password generator bug

--- script.js
+++ script.js
@@ -44,15 +44,30 @@ function getRandomIndex(list) {
   return list[randomInt(list.length)]
 }
 
-// prompt the user for an integer value, return input object
-function promptUserForInt(message) {
+// prompt the user for a specified value, and a given condition function
+function promptUserForInputType(inputType, message, isValidCondition) {
   var userInput = window.prompt(message)
-  var number = parseInt(userInput)
+  var isValidType
 
-  return userInput === null ? null : {
-    isValidType: !isNaN(number),
-    value: number
+  var inputObject = {
+    // value:...
+    // isValidType:...
+    // isValidCondition:...
+    canceled: userInput === null
   }
+
+  // validate input for number type
+  if (inputType === "number") {
+    userInput = parseInt(userInput)
+    isValidType = !isNaN(userInput)
+  }
+
+  // assign object fields
+  inputObject.isValidType = isValidType
+  inputObject.value = userInput
+  inputObject.isValidCondition = isValidType && isValidCondition(userInput)
+
+  return inputObject
 }
 
 /* ------------ */
@@ -106,21 +121,33 @@ function getRandomLetterUpper() {
 */
 function generatePassword(minLength, maxLength) {
 
-  // get password length input from user
-  var passwordLength = promptUserForInt("Enter a password length (between " + minLength + " and " + maxLength + " characters)")
+  // initialize password length var
+  var passwordLengthResult
 
-  // user exited the prompt
-  if (passwordLength === null) return
+  // get input from the user until it is validated or until they exit
+  while (true) {
+    passwordLengthResult = promptUserForInputType(
+      "number", 
+      "Enter a password length (between " + minLength + " and " + maxLength + " characters)", 
+      function(inputNumber) {
+        return inputNumber >= minLength && inputNumber <= maxLength
+      }
+    )
 
-  // handle when the user's input is not valid (if it is not a number)
-  if (!passwordLength.isValidType) {
-    window.alert("Please enter a valid number")
-    return generatePassword() // tail call
+    if (passwordLengthResult.canceled) return // user exited prompt
 
-  // handle when the user's input is outside of password length range
-  } else if (passwordLength.value < minLength || passwordLength.value > maxLength) {
-    window.alert("Password length must be within 7 and 128 characters")
-    return generatePassword() // tail call
+    // if input type is invalid (not a number)
+    if (!passwordLengthResult.isValidType) {
+      window.alert("Please enter a valid number")
+
+    // if input type is valid but does not meet the condition (password length range)
+    } else if (!passwordLengthResult.isValidCondition) {
+      window.alert("Password length must be between " + minLength + " and " + maxLength + " characters")
+
+    // else no other invalidations occur, break the prompt loop
+    } else {
+      break
+    }
   }
 
   // list of existing password options
@@ -135,9 +162,9 @@ function generatePassword(minLength, maxLength) {
   var selectedPasswordOptions = []
 
   // iterate over all existing password options, prompting the user for each one
-  for (var i = 0; i < passwordOptions.length; i++) {
-    var option = passwordOptions[i]
-    var userConfirmed = window.confirm("Would you like to include " + option.name + " in your password? (Okay = Yes, Cancel = No)")
+  for (let i = 0; i < passwordOptions.length; i++) {
+    let option = passwordOptions[i]
+    let userConfirmed = window.confirm("Would you like to include " + option.name + " in your password? (Okay = Yes, Cancel = No)")
 
     // push option to 'selectedPasswordOptions' array if the user confirms the option
     if (userConfirmed) selectedPasswordOptions.push(option)
@@ -145,14 +172,14 @@ function generatePassword(minLength, maxLength) {
 
   // if the user selected no options, choose one at random
   if (selectedPasswordOptions.length === 0) {
-    var randomOption = getRandomIndex(passwordOptions)
+    let randomOption = getRandomIndex(passwordOptions)
     window.alert("No specifications were given. Generating password with: " + randomOption.name)
     selectedPasswordOptions.push(randomOption)
   }
 
   // password generation
   var passwordBuffer = ""
-  for (var i = 0; i < passwordLength.value; i++) {
+  for (let i = 0; i < passwordLengthResult.value; i++) {
     passwordBuffer += getRandomIndex(selectedPasswordOptions).generate()
   }
 
